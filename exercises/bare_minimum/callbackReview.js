31f71fab12f9bf99b58820f21e34a23c661be1a7
Rane Gridley <ranegri3@gmail.com>
2016-03-23 14:21:38 -0700

    Complete callBackReview and PromiseConstuctor

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -18,7 +18,7 @@ var request = require('request');
 
 // fs.readFile(__dirname + '/../../README.md', 'utf8', function (err, content) {
 //   console.log('Example from callbackReview.js')
-//   if (err) { 
+//   if (err) {
 //     console.log('fs.readFile failed :(\n', err)
 //   } else {
 //     console.log('fs.readFile successfully completed :)\n', content)
@@ -36,17 +36,36 @@ var request = require('request');
 // This function should retrieve the first line of the file at `filePath`
 // HINT: Passing 'utf8' as the second argument to fs.readFile will give you a stringified file
 // HINT: You can get an array of lines by splitting on the '\n' character
-var pluckFirstLineFromFile = function (filePath
-) {
+
+    // if (err) {
+    //   console.log('fs.readFile failed :(\n', err);
+    // } else {
+    //   var lines = content.split("\n");
+    //   console.log('fs.readFile successfully completed :)\n', lines[0]);
+    // }
+var pluckFirstLineFromFile = function (filePath, cb) {
   // YOUR CODE HERE
+  fs.readFile(filePath, 'utf8', function (err, content) {
+    var firstLine;
+    if (content) {
+      firstLine = content.split("\n")[0];
+    }
+    cb(err, firstLine);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 // HINT: the `request` module has been included to help you send HTTP requests
 // HINT: there is a `statusCode` property on the `response` object
-var getStatusCode = function (url
-) {
+var getStatusCode = function (url, cb) {
   // YOUR CODE HERE
+  request(url, function(err, response) {
+    var statusCode;
+    if (response) {
+      statusCode = response.statusCode;
+    }
+    cb(err, statusCode);
+  });
 };
 
 // Export these functions so we can unit test them
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -28,11 +28,33 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function (filePath) {
   // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, 'utf8', function (err, content) {
+      if (err) {
+        reject(err);
+      } else {
+        var firstLine = content.split("\n")[0];
+        resolve(firstLine);
+      }
+    });
+  });
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function (url) {
   // TODO
+  return new Promise ( function(resolve, reject) {
+    request(url, function(err, response) {
+      if (err) {
+        reject(err);
+      } else {
+        var statusCode = response.statusCode;
+        resolve(statusCode);
+      }
+    });
+  });
+
 };
 
 // Export these functions so we can unit test them
