620c905575a4356d91b71a75f24f6ce5e1b71d40
ranebo <ranegri3@gmail.com>
2016-03-23 15:52:09 -0700

    Finish basic requirements for promise mini sprint

--- exercises/bare_minimum/chaining.js
+++ exercises/bare_minimum/chaining.js
@@ -79,14 +79,14 @@ var addNewUserToDatabaseAsync = function(user) {
 // Uncomment the lines below and run the example with `node exercises/bare_minimum/chaining.js`
 // It will succeed most of the time, but fail occasionally to demonstrate error handling
 
-// addNewUserToDatabaseAsync({ name: 'Dan', password: 'chickennuggets' })
-//   .then(function(savedUser) {
-//     console.log('All done!')
-//   })
-//   .catch(function(err) {
-//     // Will catch any promise rejections or thrown errors in the chain!
-//     console.log('Oops, caught an error: ', err.message)
-//   });
+addNewUserToDatabaseAsync({ name: 'Dan', password: 'chickennuggets' })
+  .then(function(savedUser) {
+    console.log('All done!')
+  })
+  .catch(function(err) {
+    // Will catch any promise rejections or thrown errors in the chain!
+    console.log('Oops, caught an error: ', err.message)
+  });
 
 /******************************************************************
  *                         Exercises                              *
@@ -105,7 +105,17 @@ var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return pluckFirstLineFromFileAsync(readFilePath)
+  .then(function(user) {
+    return getGitHubProfileAsync(user)
+  })
+  .then(function(body) {
+    var bodyString = JSON.stringify(body);
+    fs.writeFileSync(writeFilePath, bodyString);
+  })
+  .catch(function(err) {
+    console.log('Couldn\'t read first line =(', err);
+  });
 };
 
 module.exports = {
